7685a81b0807d3f9b67bf3288cd904e4391ace52
Nidal Souk <nidal.souk2@gmail.com>
2025-05-30 03:10:56 +0200

    Code for the Scraping and streamlit dashboard

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import subprocess
 from Scraping_VOX import Scrap_VOX
 from Scraping_UGC import Scrap_UGC  # Replace with your actual module
+import logging
 import os
+
+logging.basicConfig(
+    filename='Scraper.log',
+    filemode='w',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
 DATA_FOLDER = "data_movies"
 os.makedirs(DATA_FOLDER, exist_ok=True)
